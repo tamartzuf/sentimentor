77009880fffa3a62d1d0bb57326c1511a90774e5
tamartzuf <ttzuf1804@gmail.com>
2021-09-21 21:55:17 +0300

    style and comments

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
+//uses api to get sentiment of text
 async function sentiment(text){
     const response = await fetch("https://sentim-api.herokuapp.com/api/v1/",{
         headers: {
             'Accept': "application/json",
             "Content-Type": "application/json",
           },
-          method: "POST",
+          method: "POST", //postig data
           body: JSON.stringify({
             text: text
           }),      
     });
-    
+    //check if the response is ok
     if(response.ok) {
-      
       const result = await response.json();
       
       let res = result['result'];
@@ -20,19 +20,25 @@ async function sentiment(text){
       
       document.getElementById("polarity").textContent= pol;
       document.getElementById("type").textContent=type ;
+
+      //changes result to green
       if(type=="positive"){
         document.getElementById("polarity").style.color = "green";
         document.getElementById("type").style.color = "green";
       }
+
+      //changes result to red
       else if(type=="negative"){
         document.getElementById("polarity").style.color = "red";
         document.getElementById("type").style.color = "red";
       }
+
+      //changes result to gray
       else if(type=="neutral"){
         document.getElementById("polarity").style.color = "grey";
         document.getElementById("type").style.color = "grey";
       }
-      //hidden loading gif
+    //hidden loading gif
     typeElement.parentElement.querySelector(".loading").hidden = true;
     polarityElement.parentElement.querySelector(".loading").hidden = true;  
     }
@@ -40,6 +46,8 @@ async function sentiment(text){
       alert("HTTP-Error: " + response.status);
     }
   }
+
+  //save the value of the text input
   function hundleButtonClick(){
     let text = document.getElementById('text').value;
     sentiment(text);  
@@ -49,6 +57,7 @@ async function sentiment(text){
     polarityElement.parentElement.className = "";
     typeElement.textContent = "";
     polarityElement.textContent = "";
+
 
     typeElement.parentElement.querySelector(".loading").hidden = false;
     polarityElement.parentElement.querySelector(".loading").hidden = false;  
